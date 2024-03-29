9cec72d6423bd04a8e53da29dade945728508d83
vuson1709 <133303660+vuson1709@users.noreply.github.com>
2024-03-29 08:07:32 -0700

    style: Apply Mantine CSS library to project (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
+import "@mantine/core/styles.css"
+import { useMantineTheme, Button, TextInput, Modal } from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = useMantineTheme()
+
+  const [opened, { open, close }] = useDisclosure(false)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>UI Components:</p>
+      <ol>
+        <li>
+          {" "}
+          Button using color from theme object <br />
+          <Button color="sky-blue">This will appear on any screen size</Button>
+        </li>
+        <br />
+        <li>
+          Button with responsive design
+          <Button
+            color={theme.colors.yellow[9]}
+            fullWidth
+            className="breakpoints"
+          >
+            This will turn to red if screen size is below 576px
+          </Button>
+        </li>
+        <br />
+        <li>
+          InputText <br />
+          <TextInput size="sm" radius="xl" placeholder="Type your chat here" />
+        </li>
+        <br />
+        <li>
+          {" "}
+          Modal <br />
+          <div>
+            <Modal opened={opened} onClose={close} title="Authentication">
+              {/* Modal content */}
+            </Modal>
+            <Button onClick={open}>Open modal</Button>
+          </div>
+        </li>
+      </ol>
     </>
   )
 }
