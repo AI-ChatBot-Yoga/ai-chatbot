422dcb9c4505a24a12cafeb1772efc9dce2a0443
vuson1709 <133303660+vuson1709@users.noreply.github.com>
2024-04-17 23:10:09 +0700

    style: Add mockup data
    - Define role for ChatHistory, display bot's message on left side and user's message on right side
    - Add pre-defined data for bot when open chat

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -10,17 +10,42 @@ import {
 import { IconSend2 } from "@tabler/icons-react"
 import { ChangeEvent, useState, KeyboardEvent } from "react"
 
+type Message = {
+  message: string
+  sender: string
+}
+
 type Props = {
   onChatActivation: () => void
 }
 
 const ChatWindow = ({ onChatActivation }: Props) => {
   const [message, setMessage] = useState<string>("")
-  const [chatHistory, setChatHistory] = useState<string[]>([])
+  const [chatHistory, setChatHistory] = useState<Message[]>([
+    {
+      message: `Hello ☀️ Welcome to XXX Dry Cleaning service. Let us care for your clothes by choosing one of the following services 👇`,
+      sender: "bot",
+    },
+    {
+      message: `- Wash & Fold
+- Dry Cleaning
+- Stain Removal
+- Special Care Fabric Cleaning
+- Sewing & Alternations
+- Others
+Learn more about us 💛`,
+      sender: "bot",
+    },
+  ])
 
   const handleSendClick = () => {
     if (message.trim() !== "") {
-      setChatHistory([...chatHistory, message])
+      const newMessage = {
+        message,
+        sender: "user",
+      }
+
+      setChatHistory([...chatHistory, newMessage])
       setMessage("")
     }
   }
@@ -49,8 +74,11 @@ const ChatWindow = ({ onChatActivation }: Props) => {
 
       <ScrollArea className={styles.scrollArea}>
         {chatHistory.map((msg, index) => (
-          <Text key={index} className={styles.msgBubble} ta="right">
-            {msg}
+          <Text
+            key={index}
+            className={`${styles.msgBubble} ${msg.sender === "user" ? styles.rightSide : ""}`}
+          >
+            {msg.message}
           </Text>
         ))}
       </ScrollArea>
