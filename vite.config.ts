b9c4381d481be39c65f0576debde410e3b4faa57
Julie Lai <80876101+higherstates@users.noreply.github.com>
2024-05-11 14:17:27 +0700

    config: Add base path the deploy to Github Pages (#18)

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  base: "/ai-chatbot/", // For deploying to Github Pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Ensuring it points to the src directory
