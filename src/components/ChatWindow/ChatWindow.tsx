9565f0082e537af68e67508cd720eb9219f0a2e9
vuson1709 <133303660+vuson1709@users.noreply.github.com>
2024-05-20 05:47:29 -0700

    feat: add clear chat button and confirmation box to clear chat (#19)

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -1,4 +1,5 @@
 import styles from "./ChatWindow.module.css"
+import { useDisclosure } from "@mantine/hooks"
 import { IconSend2, IconReload, IconX } from "@tabler/icons-react"
 import { Box, Paper, TextInput, Text, ScrollArea } from "@mantine/core"
 import { ChangeEvent, useState, KeyboardEvent } from "react"
@@ -6,6 +7,7 @@ import { useAutoScrollToBottom } from "../../utils/useAutoScrollToBottom.ts"
 import { Message } from "../../types/message.ts"
 import { DEFAULT_MSG } from "../../constant/message.ts"
 import { useSessionStorage } from "../../utils/useSessionStorage.ts"
+import ConfirmationModal from "../Confirmation Modal/ConfirmationModal.tsx"
 
 type Props = {
   onChatActivation: () => void
@@ -13,6 +15,9 @@ type Props = {
 
 const ChatWindow = ({ onChatActivation }: Props) => {
   const [message, setMessage] = useState<string>("")
+
+  // this hook is used for Modal component (ConfirmationModal)
+  const [openedModal, { open, close }] = useDisclosure(false)
 
   // custom hook useSessionStorage is used to set initial value for previous chat, if there is no chat, it uses default DEFAULT_MSG as initial value. Then whenever there is new chatHistory, it is stored in sessionStorage
   const [chatHistory, setChatHistory] = useSessionStorage<Message[]>(
@@ -24,6 +29,9 @@ const ChatWindow = ({ onChatActivation }: Props) => {
   const viewport = useAutoScrollToBottom(chatHistory)
 
   const handleSendClick = () => {
+    // prevent function from running when there is no chat
+    if (!message) return
+
     if (message.trim() !== "") {
       const newMessage = {
         message,
@@ -45,13 +53,35 @@ const ChatWindow = ({ onChatActivation }: Props) => {
     }
   }
 
+  const handleClearChat = () => {
+    // prevent function from running when there is no chat
+    if (!chatHistory.find((chat) => chat.sender === "user")) {
+      close()
+      return
+    }
+
+    setChatHistory(DEFAULT_MSG)
+    close()
+  }
+
   return (
     <Paper shadow="sm" withBorder className={styles.chatWindow}>
+      <ConfirmationModal
+        openedModal={openedModal}
+        close={close}
+        onClearChat={handleClearChat}
+        modalMessage="Do you want to clear the previous chat?"
+      />
+
       <Box className={styles.chatWindowHeader}>
         <p>Conversation with AI Chatbot</p>
 
         <div className={styles.buttons}>
-          <IconReload aria-label="Reload button" className={styles.reloadBtn} />
+          <IconReload
+            aria-label="Reload button"
+            onClick={open}
+            className={styles.reloadBtn}
+          />
           <IconX
             aria-label="Close button"
             onClick={onChatActivation}
