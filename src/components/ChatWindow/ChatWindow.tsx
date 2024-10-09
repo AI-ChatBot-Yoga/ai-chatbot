778e72f02c533ff5ac800852c4898673f5b8258c
vuson1709 <133303660+vuson1709@users.noreply.github.com>
2024-10-09 14:54:45 +0700

    fix: throw an error if botId is empty

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -22,6 +22,10 @@ import SendBtn from "../SendBtn"
 
 const scriptTag = document.currentScript as HTMLScriptElement
 const botId = scriptTag?.getAttribute("botId") ?? import.meta.env.VITE_BOT_ID // If botId is not provided as attribute in script tag, use the botId from environment variables
+if (!botId)
+  throw new Error(
+    "Bot ID is not provided, thus failing to call API, please contact the admin or add botId in script tag or .env file"
+  )
 
 type Props = {
   onChatActivation: () => void
