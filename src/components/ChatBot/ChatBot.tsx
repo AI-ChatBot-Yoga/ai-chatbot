05843836e32f0d9c8de03473f1fa945aa50916d6
vuson1709 <133303660+vuson1709@users.noreply.github.com>
2024-10-25 10:20:59 +0700

    feat(ChatWindow): get botName from sessionStorage

--- src/components/ChatBot/ChatBot.tsx
+++ src/components/ChatBot/ChatBot.tsx
@@ -1,6 +1,6 @@
+import { useEffect, useState } from "react"
 import ChatWindow from "../ChatWindow"
 import ChatIcon from "../ChatIcon"
-import { useState } from "react"
 import { useChatHistory } from "@/hooks/useChatHistory"
 import PopupMessage from "../PopupMessage"
 import getBotIdFromScripTag from "@/utils/getBotIdFromScripTag"
@@ -10,19 +10,23 @@ const botId = getBotIdFromScripTag()
 const ChatBot = () => {
   const [isOpen, setIsOpen] = useState(false)
   const [isPopupVisible, setIsPopupVisible] = useState(true) // this useState ensure that the popup message will only show once. After ChatWindow is open, the popup message will not show again until user refresh the page.
+  const [businessName, setBusinessName] = useState("")
+
+  // Fetch config API when mount to get object of businessName, helperText, options,... and store in sessionStorage
+  useChatHistory(botId)
+
+  useEffect(() => {
+    const storedMsg = sessionStorage.getItem("defaultMsg")
+    if (storedMsg) {
+      const parsedMsg = JSON.parse(storedMsg)
+      setBusinessName(parsedMsg.businessName)
+    }
+  }, []) // This runs once when the component mounts
 
   function handleChatActivation() {
     setIsOpen((is) => !is)
     setIsPopupVisible(false)
   }
-
-  // Fetch config API when mount to get object of businessName, helperText, options,... and store in sessionStorage
-  useChatHistory(botId)
-
-  // Get businessName from sessionStorage
-  const businessName = sessionStorage.getItem("defaultMsg")
-    ? JSON.parse(sessionStorage.getItem("defaultMsg")!).businessName
-    : ""
 
   return (
     <>
